6846f9d73d7a85a33d6900522c38f5e47e9d2889
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-11-11 22:25:36 -0300

    Feat: add components in page home

--- src/pages/Admin/Home/index.jsx
+++ src/pages/Admin/Home/index.jsx
@@ -1,8 +1,31 @@
+import { Button } from "@material-tailwind/react";
+import { Appointments } from "../../../components/layouts/admin/appointments";
+import { ComplexNavbar } from "../../../components/layouts/admin/header";
+import LayoutAdmin from "../../../layouts/Admin";
+import { NewSchedule } from "../../../components/layouts/admin/newSchedule";
 
 export default function Home () {
   return(
-    <div>
-      Home
-    </div>
+    <LayoutAdmin>
+      
+      <ComplexNavbar/>
+        <div className="flex flex-col justify-center items-center">
+          
+          <div className="p-3 font-bold ">
+            Olá, José Erisvaldo
+          </div>
+          <div className="flex justify-end items-end w-full mr-3">
+            <NewSchedule /> 
+          </div>
+          <div className="flex flex-col m-3">
+          <h2 className="text-2xl">Dashboard de agendamento</h2>
+            <div className="sm:grid sm:grid-cols-2">
+              <Appointments />
+              <Appointments />
+            </div>
+          </div>
+          
+        </div>
+    </LayoutAdmin>
   )
 }
