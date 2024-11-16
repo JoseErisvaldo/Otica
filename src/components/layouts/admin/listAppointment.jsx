b31a56b0b83a4b57cec17df589d1b1658a52b3e4
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-11-15 23:11:23 -0300

    Feat: View ListAppointment

--- src/components/layouts/admin/listAppointment.jsx
+++ src/components/layouts/admin/listAppointment.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   Dialog,
@@ -8,84 +8,65 @@ import {
   Card,
   CardBody,
   Typography,
-  Avatar,
   Input,
 } from "@material-tailwind/react";
- 
+import { EyeIcon } from "@heroicons/react/24/outline";
+import supabaseRequest from "../../../services/api/supabaseRequest";  // Certifique-se de que a função supabaseRequest está configurada corretamente
+
 export function ListAppointment() {
-  const [open, setOpen] = React.useState(false);
- 
+  const [appointments, setAppointments] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const handleOpen = () => setOpen(!open);
 
-  const customers = [
-    {
-      name: "José Erisvaldo",
-      whatsapp: "11958790531",
-      price: 20,
-      typeOfCut: 'Corte simples'
-    },
-    {
-      name: "Gael",
-      whatsapp: "11958790531",
-      price: 15,
-      typeOfCut: 'Corte simples'
-    },
-    {
-      name: "José Erisvaldo",
-      whatsapp: "11958790531",
-      price: 30,
-      typeOfCut: 'Barba'
-    },
-    {
-      name: "José Erisvaldo",
-      whatsapp: "11958790531",
-      price: 30,
-      typeOfCut: 'Barba'
-    },
-    {
-      name: "José Erisvaldo",
-      whatsapp: "11958790531",
-      price: 30,
-      typeOfCut: 'Barba'
-    },
-    {
-      name: "José Erisvaldo",
-      whatsapp: "11958790531",
-      price: 30,
-      typeOfCut: 'Barba'
+  async function ViewAppointments() {
+    try {
+      const response = await supabaseRequest({
+        table: "lens_appointments_view",
+        method: "GET",
+      });
+      console.log(response)
+      setAppointments(response);
+    } catch (error) {
+      console.error("Erro ao exibir os dados:", error.message);
+    } finally {
+      setIsLoading(false);
     }
-  ];
- 
+  }
+
+  useEffect(() => {
+    ViewAppointments();
+  }, []);
+
   return (
     <>
-      <Button onClick={handleOpen} variant="gradient">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-        </svg>
+      <Button onClick={handleOpen} variant="gradient" className="flex items-center gap-3">
+        <span><EyeIcon className="h-5 w-5 text-gray-500" /></span>
+        Consultar agendamentos
       </Button>
       <Dialog open={open} handler={handleOpen}>
-        <div className="flex justify-between"> 
+        <div className="flex justify-between">
           <DialogHeader>Agendamentos</DialogHeader>
           <Button
-              variant="text"
-              color=""
-              onClick={handleOpen}
-              className="mr-1"
-            >
-              <span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-              </span>
-            </Button>
-          </div>
-          <DialogBody className="overflow-y-auto max-h-[60vh]">
-          <Card className="">
+            variant="text"
+            color=""
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+              </svg>
+            </span>
+          </Button>
+        </div>
+        <DialogBody className="overflow-y-auto max-h-[60vh]">
+          <Card>
             <CardBody>
               <div className="mb-4 flex items-center justify-between gap-3">
-                <Typography variant="h5" color="blue-gray" className="">
-                  <Input type="date"></Input>
+                <Typography variant="h5" color="blue-gray">
+                  <Input type="date" />
                 </Typography>
                 <Typography
                   as="a"
@@ -101,27 +82,32 @@ export function ListAppointment() {
                   </Button>
                 </Typography>
               </div>
+
               <div className="divide-y divide-gray-200">
-                {customers.map(({ name, price,whatsapp, typeOfCut }, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center justify-between pb-3 pt-3 last:pb-0"
-                  >
+                {/* Exibindo os agendamentos */}
+                {appointments.map((appointment) => (
+                  <div key={appointment.appointment_id} className="flex items-center justify-between pb-3 pt-3 last:pb-0">
                     <div className="flex items-center gap-x-3">
                       <div>
                         <Typography color="blue-gray" variant="h6">
-                          {name}
+                          {appointment.client_name}
                         </Typography>
                         <Typography variant="small" color="gray">
-                          {whatsapp}
+                          {appointment.client_whatsapp}
                         </Typography>
                         <Typography variant="small" color="gray">
-                          {typeOfCut}
+                          {appointment.appointment_date} - {appointment.appointment_time}
+                        </Typography>
+                        <Typography variant="small" color="gray">
+                          Status: {appointment.appointment_status}
+                        </Typography>
+                        <Typography variant="small" color="gray">
+                          Criado em: {new Date(appointment.appointment_created_at).toLocaleDateString('pt-BR')} às {new Date(appointment.appointment_created_at).toLocaleTimeString('pt-BR')}
                         </Typography>
                       </div>
                     </div>
                     <Typography color="blue-gray" variant="h6">
-                      R${price}
+                      {appointment.appointment_status === "confirmed" ? "Confirmado" : "Pendente"}
                     </Typography>
                   </div>
                 ))}
