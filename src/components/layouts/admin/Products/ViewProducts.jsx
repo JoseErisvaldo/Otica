c3e66e06ceff23e8d07de8881b2deb0631294110
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-12-31 14:39:37 -0300

    Feat: add  classification, gender & button edit

--- src/components/layouts/admin/Products/ViewProducts.jsx
+++ src/components/layouts/admin/Products/ViewProducts.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Button, Card, CardBody, Typography, Avatar } from "@material-tailwind/react";
 import supabaseRequest from "../../../../services/api/supabaseRequest";
-import { EyeIcon } from "@heroicons/react/24/outline";
+import { EyeIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 export default function ViewProducts() {
@@ -15,8 +15,8 @@ export default function ViewProducts() {
       const productsData = await supabaseRequest({
         table: "view_products_details",
         method: "GET",
-        limit: 30,
       });
+      console.log(productsData);
       setProducts(productsData);
     } catch (error) {
       console.error("Erro ao buscar produtos:", error.message);
@@ -46,6 +46,8 @@ export default function ViewProducts() {
                 key_products_view,
                 product_id,
                 product_name,
+                classification,
+                gender,
                 product_price,
                 product_discount_percentage,
                 discounted_price,
@@ -82,6 +84,12 @@ export default function ViewProducts() {
                     <span className="font-bold">EAN:</span> {ean || "N/A"}
                   </Typography>
                   <Typography color="blue-gray" className="mb-1">
+                    <span className="font-bold">Classificação:</span> {classification || "N/A"}
+                  </Typography>
+                  <Typography color="blue-gray" className="mb-1">
+                    <span className="font-bold">Gênero:</span> {gender || "N/A"}
+                  </Typography>
+                  <Typography color="blue-gray" className="mb-1">
                     <span className="font-bold">Preço:</span> <span className="">R$ {product_price.toFixed(2)}</span>
                   </Typography>
                   <Typography color="blue-gray" className="mb-1">
@@ -105,11 +113,17 @@ export default function ViewProducts() {
                   <Typography color="blue-gray" className="mb-1">
                     <span className="font-bold">Fornecedor:</span> {supplier_name || "N/A"}
                   </Typography>
-                  <div className="flex gap-2 w-full justify-center">
+                  <div className="flex flex-col items-center gap-2 w-full justify-center">
                     <Link to={`/admin/productsdetails/${key_products_view}`}>
-                      <Button className="flex items-center gap-2">
+                      <Button color="green" className="flex items-center gap-2">
                         <EyeIcon className="h-5 w-5 text-white" />
-                        Ver Detalhes
+                        Comprar
+                      </Button>
+                    </Link>
+                    <Link to={`/admin/editproduct/${key_products_view}`}>
+                      <Button color="blue" className="flex items-center gap-2">
+                        <PencilSquareIcon className="h-5 w-5 text-white" />
+                        Editar
                       </Button>
                     </Link>
                   </div>
