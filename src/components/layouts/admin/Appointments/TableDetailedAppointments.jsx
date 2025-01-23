8210d853310e0a09fe8d1494097bc9e23d30e0f5
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-01-23 08:39:31 -0300

    Feat: adding the schedule table view

---
+++ src/components/layouts/admin/Appointments/TableDetailedAppointments.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import {
+  Card,
+  CardBody,
+  Typography,
+
+} from "@material-tailwind/react";
+import { SeeSchedules } from "./seeSchedules";
+import { DetailsSchedules } from "./datailsSchedules";
+import Table, { TableHead, TableHeader, TableBody, TableRow, TableCell } from "../../../UI/admin/Table";
+
+export function TableDetailedAppointments({ appointments = [], title, message }) {
+
+  return (
+    <Card className="m-6 w-full">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          {title}
+        </Typography>
+
+        {appointments.length > 0 ? (
+          <Table>
+              <TableHead>
+                  <TableHeader>Data Criado</TableHeader>
+                  <TableHeader>Data do Agendamento</TableHeader>
+                  <TableHeader>Hora</TableHeader>
+                  <TableHeader>Nome do Cliente</TableHeader>
+                  <TableHeader>Status</TableHeader>
+                  <TableHeader>Ações</TableHeader>
+              </TableHead>
+              <TableBody>
+                {appointments.map((appointment) => (
+                  <TableRow key={appointment.appointment_id}>
+                    <TableCell>{new Date(appointment.appointment_created_at).toLocaleString()}</TableCell>
+                    <TableCell>{appointment.appointment_date}</TableCell>
+                    <TableCell>{appointment.appointment_time}</TableCell>
+                    <TableCell>{appointment.client_name}</TableCell>
+                    <TableCell>{appointment.status}</TableCell>
+                    <TableCell>
+                      <div className="flex  gap-3">
+                        <DetailsSchedules appointment_id={appointment.appointment_id} />
+                        <SeeSchedules lens_order_id={appointment.appointment_id} />
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <Typography>Nenhum agendamento encontrado.</Typography>
+          )}
+      </CardBody>
+
+    </Card>
+  );
+}
--- src/pages/Admin/Home/index.jsx
+++ src/pages/Admin/Home/index.jsx
@@ -1,91 +1,98 @@
-import { Card } from "@material-tailwind/react";
-import LayoutAdmin from "../../../layouts/Admin";
-import SideBar from "../../../components/layouts/admin/dashboard/sideBar";
-import PieChart from "../../../components/layouts/admin/dashboard/pieChart";
 import { useEffect, useState } from "react";
 import supabase from "../../../services/supabase";
 import { ListAppointment } from "../../../components/layouts/admin/Appointments/listAppointment";
 import { NewSchedule } from "../../../components/layouts/admin/Appointments/newSchedule";
 import { Appointments } from "../../../components/layouts/admin/Appointments/appointments";
-import StackedBarChart from "../../../components/layouts/admin/dashboard/Appointments/AppointmentMonthDoctor";
-
+import { TableDetailedAppointments } from "../../../components/layouts/admin/Appointments/TableDetailedAppointments";
+import supabaseRequest from "../../../services/api/supabaseRequest";
 export default function Home() {
   const [appointmentNextDay, setAppointmentNextDay] = useState([]);
   const [appointmentsToday, setAppointmentsToday] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+  console.log(appointments);
   const [loading, setLoading] = useState(true);
 
-  const fetchAppointmentsNextDay = async () => {
+  async function fetchAppointmentsNextDay() {
     try {
       setLoading(true);
-      let { data: view_appointments_next_day, error } = await supabase
-        .from("view_appointments_next_day")
-        .select("*");
-      if (error) {
-        throw new Error(error.message);
-      }
-      setAppointmentNextDay(view_appointments_next_day);
+      const response = await supabaseRequest({
+        table: "view_appointments_next_day",
+        method: "GET",
+      });
+  
+      setAppointmentNextDay(response);
     } catch (error) {
-      console.error("Erro ao buscar agendas:", error.message);
+      console.error("Erro ao buscar agendas do próximo dia:", error.message);
     } finally {
       setLoading(false);
     }
-  };
-
-  const fetchAppointmentsToday = async () => {
+  }
+  
+  async function fetchAppointmentsToday() {
     try {
       setLoading(true);
-      let { data: view_appointments_today, error } = await supabase
-        .from("view_appointments_today")
-        .select("*");
-      if (error) {
-        throw new Error(error.message);
-      }
-      setAppointmentsToday(view_appointments_today);
+      const response = await supabaseRequest({
+        table: "view_appointments_today",
+        method: "GET",
+      });
+  
+      setAppointmentsToday(response);
     } catch (error) {
-      console.error("Erro ao buscar agendas:", error.message);
+      console.error("Erro ao buscar agendas de hoje:", error.message);
     } finally {
       setLoading(false);
     }
-  };
+  }
+  
+  async function fetchAppointments() {
+    try {
+      setLoading(true);
+      const response = await supabaseRequest({
+        table: "view_detailed_appointments",
+        method: "GET",
+      });
+  
+      setAppointments(response);
+    } catch (error) {
+      console.error("Erro ao buscar todas as agendas:", error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+  
 
   useEffect(() => {
     fetchAppointmentsNextDay();
     fetchAppointmentsToday();
+    fetchAppointments();
   }, []);
 
   return (
-    <LayoutAdmin>
-      <div className="flex flex-col justify-center items-center">
-        <div className="p-3 font-bold text-3xl">Olá, José Erisvaldo</div>
-        <div className="flex justify-end items-end w-full mr-3 gap-3">
+    <div className="w-full">
+      <div className="flex flex-col">
+        <div className="flex justify-end items-end flex-wrap w-full mr-3 gap-3">
           <ListAppointment />
           <NewSchedule />
         </div>
         <div className="flex flex-col m-3">
-          <h2 className="text-2xl font-bold">Dashboard de agendamento</h2>
-          <div className="sm:grid sm:grid-cols-2">
+          <h2 className="text-2xl font-bold text-start">Detalhes dos agendamentos</h2>
+          <div className="flex flex-wrap">
             <Appointments
-              title={"Agendamento hoje"}
+              title={"Agendamentos hoje"}
               appointments={appointmentsToday}
               message={"Olá, estou passando aqui para lembra do seu agendamento de hoje"}
             />
             <Appointments
-              title={"Agendamento para amanhã"}
+              title={"Agendamentos amanhã"}
               appointments={appointmentNextDay}
               message={"estou passando aqui para lembra do seu agendamento para amanhã"}
             />
           </div>
         </div>
         <div className="sm:flex">
-          <div>
-            <SideBar />
-          </div>
-          <div>
-            <PieChart />
-          </div>
-          <StackedBarChart />
+          <TableDetailedAppointments appointments={appointments} />
         </div>
       </div>
-    </LayoutAdmin>
+    </div>
   );
 }
